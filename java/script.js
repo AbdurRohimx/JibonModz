7504db8304dc8a074b5fcfbb5f0aa2ec7887a98a
Rohim Bhai <108090636+AbdurRohimx@users.noreply.github.com>
2023-03-27 05:22:45 +0600

    Jibon Modz By Rohim Bhai

---
+++ java/script.js
@@ -0,0 +1,63 @@
+// worklist btn overlay
+
+// function openForm() {
+//   document.getElementById("popup1").style.display = "block";
+// }
+// function closeForm() {
+//   document.getElementById("popup1").style.display = "none";
+// }
+
+// home logo click
+const resetButton = document.querySelector('#reset-button');
+resetButton.addEventListener('click', function() {
+  location.reload();
+});
+
+// parson logo click
+const whatsappButton = document.querySelector('#whatsapp-button');
+whatsappButton.addEventListener('click', function() {
+  window.open('https://web.whatsapp.com/send?phone=+8801404345698');
+});
+
+const toolButton = document.querySelector('#tool-button');
+toolButton.addEventListener('click', function() {
+  window.open('tool.html');
+});
+
+const profileButton = document.querySelector('#profile-button');
+profileButton.addEventListener('click', function() {
+  window.open('profile.html');
+});
+
+
+
+// psteam button click
+function psteam() {
+  window.open("List-Page/Psteam.html");
+}
+
+function ffh4x() {
+  window.open("List-Page/ffh4x.html");
+}
+
+function newmod() {
+  window.open("List-Page/newmod.html");
+}
+function lkteam() {
+  window.open("List-Page/lkteam.html");
+}
+function ifinity() {
+  window.open("List-Page/ifinity.html");
+}
+function broken() {
+  window.open("List-Page/broken.html");
+}
+function darknet() {
+  window.open("List-Page/darknet.html");
+}
+function psh4x() {
+  window.open("List-Page/psh4x.html");
+}
+function brmods() {
+  window.open("List-Page/brmods.html");
+}
---
+++ java/tool.js
@@ -0,0 +1,83 @@
+const button = document.getElementById("vphone");
+button.addEventListener("click", function() {
+  const link = document.createElement("a");
+  link.download = "myfile.pdf";
+  link.href = "https://apkadmin.com/qx9n2jy9ykf3/VPHONE-GOLD.apk.html";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
+const button1 = document.getElementById("vmos");
+button1.addEventListener("click", function() {
+  const link = document.createElement("a");
+  link.download = "myfile.pdf";
+  link.href = "https://apkadmin.com/i6r8fxfj18sv/VMOS_pro.apk.html";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
+const button2 = document.getElementById("vm");
+button2.addEventListener("click", function() {
+  const link = document.createElement("a");
+  link.download = "myfile.pdf";
+  link.href = "https://apkadmin.com/ityh01t62v1d/F1VM_INFINITY_VIP.apk.html";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
+
+const button3 = document.getElementById("data");
+button3.addEventListener("click", function() {
+  const link = document.createElement("a");
+  link.download = "myfile.pdf";
+  link.href = "https://apkadmin.com/9um3e6y9lmed/Data_Fix_Black.zip.html";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
+
+const button4 = document.getElementById("bluestacks");
+button4.addEventListener("click", function() {
+  const link = document.createElement("a");
+  link.download = "myfile.pdf";
+  link.href = "https://www.bluestacks.com/bluestacks-5.html";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
+
+const button5 = document.getElementById("guerdian");
+button5.addEventListener("click", function() {
+  const link = document.createElement("a");
+  link.download = "myfile.pdf";
+  link.href = "https://gameguardian.net/forum/files/file/2-gameguardian/";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
+
+const button6 = document.getElementById("duel");
+button6.addEventListener("click", function() {
+  const link = document.createElement("a");
+  link.download = "myfile.pdf";
+  link.href = "https://play.google.com/store/apps/details?id=com.ludashi.dualspace&hl=en&gl=US";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
+const button7 = document.getElementById("peralal");
+button7.addEventListener("click", function() {
+  const link = document.createElement("a");
+  link.download = "myfile.pdf";
+  link.href = "https://play.google.com/store/apps/details?id=com.lbe.parallel.intl&hl=en&gl=US";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
